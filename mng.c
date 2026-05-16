ff6a8adc3bc769f49da7a6cb20cb4ab5181c5cf6
girvel <widauka@ya.ru>
2026-05-16 19:28:08 +0500

    Define a bit of structure

--- mng.c
+++ mng.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#define MNG_IMPLEMENTATION
+#include "mng.h"
 
-int main() {
+// Concept: mng.h is a single-header library
+// I'm not sure where would configuration files go/who would run them, but for now we'll test in
+// mng.c
+
+int
+main()
+{
     printf("Hello, world!\n");
     return 0;
 }
