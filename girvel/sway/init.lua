08fd7f0c534ac52c9bb8aeacf92744f097ca7528
girvel <widauka@ya.ru>
2026-08-18 01:48:18 +0500

    Fix sway keyboard issues, some improvements to gjoll

--- girvel/sway/init.lua
+++ girvel/sway/init.lua
@@ -2,13 +2,14 @@ local mng = require("mng")
 
 mng.package [[
   dbus elogind polkit
-  sway
+  sway xwayland-satellite
   pipewire wireplumber alsa-utils pavucontrol
-  fuzzel wl-clipboard
+  fuzzel wl-clipboard wl-clip-persist
   ttf-ubuntu-font-family dejavu-fonts-ttf
+  zramen
 ]]
 
-mng.service_on("dbus", "polkitd")
+mng.service_on("dbus", "polkitd", "zramen")
 
 mng.as_user("girvel", function()
   mng.symlink("~/.config/sway/config", "./sway/config")
